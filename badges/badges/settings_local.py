dfd1e2634b11ade2740f1f031ce7408fbe2f8733
Dirk Uys <dirkcuys@gmail.com>
2013-02-27 21:06:28 +0200

    Use local settings file

---
+++ badges/badges/settings_local.py
@@ -0,0 +1,12 @@
+from settings import *
+
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1', )
--- badges/badges/wsgi.py
+++ badges/badges/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "badges.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "badges.settings_local")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
