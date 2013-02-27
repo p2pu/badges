86139b5a3db4da1ce0dab391480a19c7227d33fe
Dirk Uys <dirkcuys@gmail.com>
2013-02-27 14:01:25 +0200

    update badge test - more complete now

--- badges/badge/tests.py
+++ badges/badge/tests.py
@@ -6,47 +6,78 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from badges import api as badge_api
+from badge import api as badge_api
 
 
 class SimpleTest(TestCase):
+    
+    def setUp(self):
+        self.badge_attrs = [
+            'uri',
+            'title',
+            'image_uri',
+            'description',
+            'requirements',
+            'author_uri'
+        ]
+
+        self.badge_values = [
+            'Movie Maker',
+            '/uri/image/1',
+            'Create a short movie',
+            'Create a movie and upload it to youtube or vimeo',
+            '/uri/user/1',
+        ]
+
 
     def test_create_and_get_badge(self):
         """ Test that we can create a badge """
-        title = "Movie Maker"
-        image_uri = ""
-        description = "Create a short movie"
-        requirements = ""
-        badge = badge_api.create_badge(title, image_uri, description, requirements)
-
-        badge_attr = ['uri', 'image_uri', 'title', 'description', 'requirements', 'author_uri']
+        badge = badge_api.create_badge(*self.badge_values)
 
         # test the presence or attributes for a badge
-        for attr in badge_attr:
+        for attr in self.badge_attrs:
             self.assertIn(attr, badge)
+
+        # test attribute values
+        attrs = self.badge_attrs
+        del(attrs[0])
+        for key, value in zip(attrs, self.badge_values):
+            self.assertEquals(badge[key], value)
        
+        # test make sure attributes are equal
         badge2 = badge_api.get_badge(badge['uri'])
+        self.assertEqual(badge, badge2)
 
-        # make sure attributes are equal
-        for attr in badge_attr:
-            self.assertEqual(badge[attr], badge2[attr])
-
-
-    def get_badges(self):
-        """ Test that we can create and retrieve multiple badges """
-
-        badges = badge_api.get_badges()
-        self.assertTrue(len(badges) == 0)
+        # test that the badge shows up in drafts
+        badges = get_user_draft_badges(badge['author_uri'])
+        self.assertEquals(len(badges), 1)
+        self.assertEquals(badge2, badges[0])
 
 
     def test_update_badge(self):
         """ Test that we can update a badge """
-        pass
+        badge = badge_api.create_badge(*self.badge_values)
+        kwargs = dict(zip(self.badge_attrs, [badge['uri']] + self.badge_values))
+        del kwargs['author_uri']
+        kwargs['title'] = 'A new title'
+        badge_api.update_badge(**kwargs)
+        badge2 = badge_api.get_badge(badge['uri'])
+        self.assertNotEquals(badge, badge2)
 
 
     def test_publish_badge(self):
+        badge = badge_api.create_badge(*self.badge_values)
 
-        badge = badge_api.create_badge()
-        badge = badge_api.publish_badge(badge['uri'])
-        with self.assertRaises(Exception):
-            badge_api.update_badge(badge['uri'], title="Updated title")
+        badges = badge_api.get_published_badges()
+        self.assertTrue(len(badges) == 0)
+        badges = badge_api.get_user_draft_badges(badge['uri'])
+        self.assertTrue(len(badges) == 1)
+
+        badge_api.publish_badge(badge['uri'])
+        self.assertRaises(Exception, badge_api.update_badge, [badge['uri']], {'title':'Updated title'})
+        badges = badge_api.get_published_badges()
+        self.assertTrue(len(badges) == 1)
+        badges = badge_api.get_user_draft_badges(badge['uri'])
+        self.assertTrue(len(badges) == 0)
+
+
