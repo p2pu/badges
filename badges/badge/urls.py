6d8c79916196f0d1f42a565c152ac619ecf01257
Dirk Uys <dirkcuys@gmail.com>
2013-02-28 11:36:03 +0200

    Upload image and handle model errors for badge creation

--- badges/badge/urls.py
+++ badges/badge/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-from badge.views import BadgeCreateView
 
 urlpatterns = patterns('',
-    url(r'^create/$', BadgeCreateView.as_view(), name='badge_create'),
+    url(r'^create/$', 'badge.views.create', name='badge_create'),
     url(r'^preview/(?P<badge_id>[\d]+)/$', 'badge.views.preview', name='badge_preview'),
     url(r'^publish/(?P<badge_id>[\d]+)/$', 'badge.views.publish', name='badge_publish'),
     url(r'^view/(?P<badge_id>[\d]+)/$', 'badge.views.view', name='badge_view'),
--- badges/badge/views.py
+++ badges/badge/views.py
@@ -3,37 +3,40 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
 
 from badge.forms import BadgeForm
 from badge import api as badge_api
-
-class BadgeCreateView(FormView):
-    template_name = 'badge/create.html'
-    form_class = BadgeForm
-    success_url = '/badges/'
-
-    def form_valid(self, form):
-        #TODO upload image
-        badge = badge_api.create_badge(
-            form.cleaned_data['title'],
-            '/uri/image/1',
-            form.cleaned_data['description'],
-            form.cleaned_data['requirements'],
-            '/uri/author/1'
-        )
-        #TODO handle possible error from model
-        return http.HttpResponseRedirect(
-            reverse('badge_preview', args=(badge_api.uri2id(badge['uri']),))
-        )
-
+from media import models as media_api
 
 def create( request ):
-    context = {}
+    template_name = 'badge/create.html'
+
+    if request.method == 'POST':
+        form = BadgeForm(request.POST, request.FILES)
+    else:    
+        form = BadgeForm()
+
+    if form.is_valid():
+        #TODO upload image
+        image = media_api.upload_image(request.FILES['image'], '/uri/user/1')
+        try:
+            badge = badge_api.create_badge(
+                form.cleaned_data['title'],
+                image['uri'],
+                form.cleaned_data['description'],
+                form.cleaned_data['requirements'],
+                '/uri/user/1'
+            )
+            return http.HttpResponseRedirect(
+                reverse('badge_preview', args=(badge_api.uri2id(badge['uri']),))
+            )
+        except badge_api.DuplicateTitleError:
+            form.errors['title'] = [_('Badge title needs to be unique'),]
+
     return render_to_response(
-        'badge/create.html',
-        context,
-        context_instance=RequestContext(request)
-    )
+        template_name, {'form': form},
+        context_instance=RequestContext(request))
 
 
 def preview( request, badge_id ):
@@ -41,6 +44,7 @@ def preview( request, badge_id ):
     context = {
         'badge': badge_api.get_badge(badge_api.id2uri(badge_id))
     }
+    context['badge']['image'] = media_api.get_image(context['badge']['image_uri'])
 
     return render_to_response(
         'badge/preview.html',
