13d7ea644be7a007d2d0098f15839b114bddbce8
Dirk Uys <dirkcuys@gmail.com>
2013-03-01 16:57:03 +0200

    Display published badges on landing page

--- badges/landing/views.py
+++ badges/landing/views.py
@@ -2,14 +2,12 @@ from django import http
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from badge import api as badge_api
+
 def home(request):
     context = {}
-    context['badges'] = [
-        {'title': 'Content Marketing'},
-        {'title': 'Movie Maker'},
-        {'title': 'Science Field Trip'},
-        {'title': 'Open Licensee'},
-    ]
+    context['badges'] = badge_api.get_published_badges()
+
     context['projects'] = [
         {'title': 'IPhone movie'},
         {'title': 'Robots in Love'},
