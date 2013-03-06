66bc5ebb98cf83d93a8c7c31059650bd7e8e8654
Dirk Uys <dirkcuys@gmail.com>
2013-03-06 12:40:58 +0200

    update settings_local.dist file

--- badges/badges/settings_local.dist.py
+++ badges/badges/settings_local.dist.py
@@ -4,6 +4,9 @@ from settings import *
 INSTALLED_APPS += ( 'debug_toolbar',)
 MIDDLEWARE_CLASSES += ( 'debug_toolbar.middleware.DebugToolbarMiddleware',)
 INTERNAL_IPS = ('127.0.0.1', )
+DEBUG_TOOLBAR_CONFIG = { 'INTERCEPT_REDIRECTS': False }
+
+INSTALLED_APPS += ( 'testdata', )
 
 OAUTH_GRANT_URL = 'http://host.org/oauth/authorize/'
 OAUTH_TOKEN_URL = 'http://host.org/oauth/token/'
