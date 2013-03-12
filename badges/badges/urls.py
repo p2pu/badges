be42523c15a480bbcd919fb1ba18e0799267a008
Dirk Uys <dirkcuys@gmail.com>
2013-03-12 23:18:03 +0200

    Remove admin urls from badges.urls

--- badges/badges/urls.py
+++ badges/badges/urls.py
@@ -16,7 +16,7 @@ urlpatterns = i18n_patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', include('landing.urls')),
     url(r'^badge/', include('badge.urls')),
