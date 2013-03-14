c6bf6d239f38e0311d1fc6fb2430f5971bd1f098
Dirk Uys <dirkcuys@gmail.com>
2013-03-14 14:34:35 +0200

    Create users for test data. Some extra data for testing

--- badges/testdata/models.py
+++ badges/testdata/models.py
@@ -3,6 +3,7 @@ from django.core.files import File
 
 from media import models as media_api
 from badge import models as badge_api
+from p2pu_user import models as p2pu_user_api
 import os
 
 def load_test_data(data_file):
@@ -10,6 +11,10 @@ def load_test_data(data_file):
     root = os.path.dirname(os.path.abspath(data_file))
     test_data = simplejson.load(df)
     for badge in test_data['badges']:
+        p2pu_user_api.save_user(
+            p2pu_user_api.uri2username(badge['author_uri']), 
+            'http://placehold.it/150x150'
+        )
         with open(os.path.join(root, badge['image']), 'rb') as image_file:
             image = media_api.upload_image(File(image_file), badge['author_uri'])
         badge['image_uri'] = image['uri']
