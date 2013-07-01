d7a0e44c451d1e6a9fc0565a058aa6c9b65c7ba8
ercchy <erika@serox.si>
2013-07-01 23:15:18 +0200

    changed verification of domain

--- badges/services/procesors.py
+++ badges/services/procesors.py
@@ -1,5 +1,4 @@
 from urlparse import urlparse
-from django.contrib.sites.models import Site
 from django.conf import settings
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
@@ -18,8 +17,6 @@ def check_if_url_is_valid(url):
     if domain.split('.')[0] == 'www':
         domain = '.'.join(domain.split('.')[1:])
 
-    if settings.DEBUG:
-        return True
-    elif domain != Site.objects.get_current():
+    if domain != settings.ORGANISATION_URL:
         return False
     return True
