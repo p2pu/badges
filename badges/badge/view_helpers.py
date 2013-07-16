1b81815acfc7b0efa20612b0254aa11cd7f3190e
ercchy <erika@serox.si>
2013-07-16 16:56:26 +0200

    fixing social shoutout

--- badges/badge/view_helpers.py
+++ badges/badge/view_helpers.py
@@ -1,7 +1,10 @@
+from django.core.urlresolvers import reverse
+from django.conf import settings
 from media import models as media_api
 from p2pu_user import models as p2pu_user_api
 
 def fetch_badge_resources( badge ):
     badge['image'] = media_api.get_image(badge['image_uri'])
     badge['author'] = p2pu_user_api.get_user(badge['author_uri'])
+    badge['url'] = settings.ORGANISATION_URL + reverse('badge_view', args=(badge['id'],))
     return badge
