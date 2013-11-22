16d1d5794c8e1590cde61edeea89c439387d0404
ercchy <erika@serox.si>
2013-11-22 16:16:29 +0100

    improved filtering of badges on browsing

--- badges/landing/urls.py
+++ badges/landing/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'landing.views.home', name='landing'),
     url(r'^search$', 'landing.views.search', name='search'),
-    url(r'^browse', 'landing.views.browse_all_badges', name='browse_all_badges'),
+    #url(r'^browse', 'landing.views.browse_all_badges', name='browse_all_badges'),
+    url(r'^browse/(?P<option>[\w]+)/', 'landing.views.browse_badges', name='browse_badges'),
 )
