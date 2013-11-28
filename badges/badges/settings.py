9989118b9ed2ac4923002c929ed25a1dbabf89e2
ercchy <erika@serox.si>
2013-11-28 10:00:18 +0100

    cleaning out settings file

--- badges/badges/settings.py
+++ badges/badges/settings.py
@@ -16,94 +16,53 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': path('db.sqlite'),       # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': path('db.sqlite'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.p2pu.org'
+]
+TIME_ZONE = 'Europe/Amsterdam'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Africa/Johannesburg'
+MEDIA_ROOT = path('../uploads')
+MEDIA_URL = '/media/'
+STATIC_ROOT = path('../staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, '../static'),
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 gettext = lambda s: s
-
 LANGUAGES = (
     ('en', gettext('English')),
     ('en-us', gettext('English-American')),
 )
 
-SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = False
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: '/home/media/media.lawrence.com/media/'
-MEDIA_ROOT = path('../uploads')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: 'http://media.lawrence.com/media/', 'http://example.com/media/'
-MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' 'static/' subdirectories and in STATICFILES_DIRS.
-# Example: '/home/media/media.lawrence.com/static/'
-STATIC_ROOT = path('../staticfiles')
-
-# URL prefix for static files.
-# Example: 'http://media.lawrence.com/static/'
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like '/home/html/static' or 'C:/www/django/static'.
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    #path('../static'),
-    os.path.join(PROJECT_DIR, '../static'),
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = '(auvykgyxsa=bv16j75p_p90jqx%d54)u=ilx2m0id-g-vg+eh'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -113,8 +72,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -131,13 +89,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 ROOT_URLCONF = 'badges.urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'badges.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like '/home/html/django_templates' or 'C:/www/django/templates'.
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     path('../templates'),
 )
 
@@ -167,13 +121,9 @@ INSTALLED_APPS = (
     'oembed',
 )
 
-#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+#############################################################################
+# Logging settings
+#############################################################################
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -203,6 +153,10 @@ LOGGING = {
         },
     }
 }
+
+#############################################################################
+# OAuth settings
+#############################################################################
 
 OAUTH_GRANT_URL = ''
 OAUTH_TOKEN_URL = ''
