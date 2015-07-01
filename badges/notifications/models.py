02ff09f293e593df35d609b95706d57ba9c36ad6
ercchy <erika@serox.si>
2015-07-01 12:27:10 +0200

    converting indents to spaces

--- badges/notifications/models.py
+++ badges/notifications/models.py
@@ -10,38 +10,38 @@ log = logging.getLogger(__name__)
 
 
 def send_notification_i18n(receiver_uri, subject_template, text_template, html_template=None, context={}, sender=None, callback=None):
-	"""
-	Translate the notification before sending it
-	"""
-	subject = localize_for_user(receiver_uri, subject_template, context).strip()
-	text = localize_for_user(receiver_uri, text_template, context).strip()
-	html = None
-	if html_template:
-		html = localize_for_user(receiver_uri, html_template, context).strip()
+    """
+    Translate the notification before sending it
+    """
+    subject = localize_for_user(receiver_uri, subject_template, context).strip()
+    text = localize_for_user(receiver_uri, text_template, context).strip()
+    html = None
+    if html_template:
+        html = localize_for_user(receiver_uri, html_template, context).strip()
 
-	return send_notification(receiver_uri, subject, text, html, sender, callback)
+    return send_notification(receiver_uri, subject, text, html, sender, callback)
 
 
 def send_notification(receiver_uri, subject, text, html=None, sender=None, callback=None):
-	"""
-	Send a notification to a single receiver via the Django-mandrill to a user
-	receiver - uri of user to send the notification to
-	sender - name to use in from part of email
-	"""
-	receiver = get_user(receiver_uri)
+    """
+    Send a notification to a single receiver via the Django-mandrill to a user
+    receiver - uri of user to send the notification to
+    sender - name to use in from part of email
+    """
+    receiver = get_user(receiver_uri)
 
-	msg = EmailMultiAlternatives(
-		subject=subject,
-		body=text,
-		from_email=settings.DEFAULT_FROM_EMAIL,
-		to=[receiver['email']],
-	)
+    msg = EmailMultiAlternatives(
+        subject=subject,
+        body=text,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[receiver['email']],
+    )
 
-	if html:
-		msg.attach_alternative(html, "text/html")
+    if html:
+        msg.attach_alternative(html, "text/html")
 
-	try:
-		msg.send()
-	except BadHeaderError:
-		log.error(u'Could not send email notification to {0}, Invalid header found.'.format(receiver['email']))
+    try:
+        msg.send()
+    except BadHeaderError:
+        log.error(u'Could not send email notification to {0}, Invalid header found.'.format(receiver['email']))
 
