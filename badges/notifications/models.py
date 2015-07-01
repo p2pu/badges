bd606f993e305a978f309327a8709b6923054a87
ercchy <erika@serox.si>
2015-07-01 12:14:46 +0200

    fixing the tests to remove the smelly code

--- badges/notifications/models.py
+++ badges/notifications/models.py
@@ -1,36 +1,32 @@
-from smtplib import SMTPException
 from django.conf import settings
-from django.utils import simplejson
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 
 from notifications.utils import localize_for_user
-
-import requests
-
-import logging
 from p2pu_user.models import get_user
 
+import logging
 log = logging.getLogger(__name__)
 
 
-def send_notification_i18n( receiver_uri, subject_template, text_template, html_template=None, context={}, sender=None, callback=None):
-	""" translate the notification before sending it """
-
+def send_notification_i18n(receiver_uri, subject_template, text_template, html_template=None, context={}, sender=None, callback=None):
+	"""
+	Translate the notification before sending it
+	"""
 	subject = localize_for_user(receiver_uri, subject_template, context).strip()
 	text = localize_for_user(receiver_uri, text_template, context).strip()
 	html = None
 	if html_template:
 		html = localize_for_user(receiver_uri, html_template, context).strip()
 
-	#if settings.DEBUG:
-	#    return
 	return send_notification(receiver_uri, subject, text, html, sender, callback)
 
 
-def send_notification( receiver_uri, subject, text, html=None, sender=None, callback=None):
-	""" Send a notification to a single receiver via the P2PU API to a user
-		receiver - uri of user to send the notification to
-		sender - name to use in from part of email
+def send_notification(receiver_uri, subject, text, html=None, sender=None, callback=None):
+	"""
+	Send a notification to a single receiver via the Django-mandrill to a user
+	receiver - uri of user to send the notification to
+	sender - name to use in from part of email
 	"""
 	receiver = get_user(receiver_uri)
 
@@ -44,11 +40,8 @@ def send_notification( receiver_uri, subject, text, html=None, sender=None, call
 	if html:
 		msg.attach_alternative(html, "text/html")
 
-	# Send it:
 	try:
 		msg.send()
-		log.error(u'Could not send email notification to {0}'.format(receiver_uri))
-		return True
-	except SMTPException:
-		return False
+	except BadHeaderError:
+		log.error(u'Could not send email notification to {0}, Invalid header found.'.format(receiver['email']))
 
--- badges/notifications/tests.py
+++ badges/notifications/tests.py
@@ -1,45 +1,47 @@
-from django.test import TestCase
+from django.http import Http404
 from mock import patch
+from django.test import TestCase
+from django.core import mail
 
-from notifications import models
+from notifications import models as notification
 from p2pu_user.models import save_user
 
 
 class SimpleTest(TestCase):
 
-	def setUp(self):
-		self.user = save_user('testuser', 'http://placehold.it', 'erika@p2pu.org')
+    def setUp(self):
+        self.user = save_user('testuser', 'http://placehold.it', 'erika@p2pu.org')
 
-	def test_send_notification(self):
+    def test_send_notification(self):
 
-		ret = models.send_notification(
-			self.user['uri'],
-			'Notification subject',
-			'Notification text',
-			'<html>Some HTML</html>',
-			'The Badge Sender'
-		)
-		self.assertTrue(ret)
+        notification.send_notification(
+            self.user['uri'],
+            'Notification subject',
+            'Notification text',
+            '<html>Some HTML</html>',
+            'The Badge Sender'
+        )
+        self.assertEquals(len(mail.outbox), 1)
+        self.assertEquals(mail.outbox[0].subject, 'Notification subject')
 
+    def test_send_notification_fail(self):
 
-	def test_send_notification_fail(self):
-		ret = models.send_notification(
-			self.user['uri'],
-			'Notification subject',
-			'Notification text',
-			'<html>Some HTML</html>',
-			'The Badge Sender'
-		)
-		self.assertTrue(ret)
+        self.assertRaises(Http404, notification.send_notification,
+                          'non/existent/user/',
+                          'Notification subject',
+                          'Notification text',
+                          '<html>Some HTML</html>',
+                          'The Badge Sender')
 
+        self.assertEquals(len(mail.outbox), 0)
 
-	def test_send_notification_i18n(self):
-		with patch('notifications.models.send_notification') as send_notification:
-			models.send_notification_i18n(
-				'/uri/user/testuser',
-				'emails/project_submitted_subject.txt',
-				'emails/project_submitted.txt',
-				context={'test1': 'test123'},
-				callback='http://call.me'
-			)
-			self.assertTrue(send_notification.called)
+    def test_send_notification_i18n(self):
+        with patch('notifications.models.send_notification') as send_notification:
+            notification.send_notification_i18n(
+                '/uri/user/testuser',
+                'emails/project_submitted_subject.txt',
+                'emails/project_submitted.txt',
+                context={'test1': 'test123'},
+                callback='http://call.me'
+            )
+            self.assertTrue(send_notification.called)
